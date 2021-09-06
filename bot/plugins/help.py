24381aaa453598a455468cc3d6755325e3b64bdb
Moedyiu <86594262+gillommnathony@users.noreply.github.com>
2021-09-06 11:50:08 +0630

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -41,8 +41,8 @@ def map(pos):
 
         button = [
             [
-             InlineKeyboardButton(text = 'Support Chat', url = "https://t.me/lkhitech"),
-             InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/kjeymax/GDUPLOAD_BOT2")
+             InlineKeyboardButton(text = 'Support Chat', url = "https://t.me/moedyiu"),
+             InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/gillommnathony/GdriveUploaderBotV3")
             ],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
 
