a90bbe305b1fd687d2ad42070add83bbe5e620f6
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-16 22:47:13 +0630

    Update idsDB.py

--- bot/helpers/sql_helper/idsDB.py
+++ bot/helpers/sql_helper/idsDB.py
@@ -1,14 +1,19 @@
 from bot.helpers.sql_helper import parent_id
 
+PARENT_ID_KEY_PREFIX = "parent_id_"
+
 def search_parent(chat_id):
-    parent_id_value = parent_id.get(chat_id)
+    parent_id_key = PARENT_ID_KEY_PREFIX + chat_id
+    parent_id_value = redis_client.get(parent_id_key)
     if parent_id_value is not None:
         return parent_id_value.decode('utf-8')
     else:
         return "root"
 
 def _set(chat_id, parent_id_value):
-    parent_id.set(chat_id, parent_id_value)
+    parent_id_key = PARENT_ID_KEY_PREFIX + chat_id
+    redis_client.set(parent_id_key, parent_id_value)
 
 def _clear(chat_id):
-    parent_id.delete(chat_id)
+    parent_id_key = PARENT_ID_KEY_PREFIX + chat_id
+    redis_client.delete(parent_id_key)
