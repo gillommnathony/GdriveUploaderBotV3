619b2e179c9dcf9036b657143aee191b0ac52046
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-16 15:52:15 +0630

    Update gDriveDB.py

--- bot/helpers/sql_helper/gDriveDB.py
+++ bot/helpers/sql_helper/gDriveDB.py
@@ -1,46 +1,27 @@
 import pickle
 import threading
-from sqlalchemy import BigInteger, Column, LargeBinary
-from bot.helpers.sql_helper import BASE, SESSION
-
-class gDriveCreds(BASE):
-    __tablename__ = "gDrive"
-    chat_id = Column(BigInteger, primary_key=True)
-    credential_string = Column(LargeBinary)
-
-
-    def __init__(self, chat_id):
-        self.chat_id = chat_id
-
-
-gDriveCreds.__table__.create(checkfirst=True)
+from bot.helpers.sql_helper import gDrive
 
 INSERTION_LOCK = threading.RLock()
 
 def _set(chat_id, credential_string):
     with INSERTION_LOCK:
-        saved_cred = SESSION.query(gDriveCreds).get(chat_id)
-        if not saved_cred:
-            saved_cred = gDriveCreds(chat_id)
-
-        saved_cred.credential_string = pickle.dumps(credential_string)
-
-        SESSION.add(saved_cred)
-        SESSION.commit()
+        filter_query = {'_id': chat_id}
+        credential_string_pickle = pickle.dumps(credential_string)
+        update_query = {'$set': {'credential_string': credential_string_pickle}}
+        gDrive.update_one(filter_query, update_query, upsert=True)
 
 
 def search(chat_id):
     with INSERTION_LOCK:
-        saved_cred = SESSION.query(gDriveCreds).get(chat_id)
+        saved_cred = gDrive.find_one({'_id': chat_id})
         creds = None
         if saved_cred is not None:
-            creds = pickle.loads(saved_cred.credential_string)
+            credential_string_pickle = saved_cred['credential_string']
+            creds = pickle.loads(credential_string_pickle)
         return creds
 
 
 def _clear(chat_id):
     with INSERTION_LOCK:
-        saved_cred = SESSION.query(gDriveCreds).get(chat_id)
-        if saved_cred:
-            SESSION.delete(saved_cred)
-            SESSION.commit()
+        gDrive.delete_one({'_id': chat_id})
