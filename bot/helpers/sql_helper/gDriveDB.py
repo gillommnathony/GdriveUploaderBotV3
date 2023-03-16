54b7344a10e089821e3552a12a52a062096e1550
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-16 22:28:38 +0630

    Update gDriveDB.py

--- bot/helpers/sql_helper/gDriveDB.py
+++ bot/helpers/sql_helper/gDriveDB.py
@@ -6,22 +6,17 @@ INSERTION_LOCK = threading.RLock()
 
 def _set(chat_id, credential_string):
     with INSERTION_LOCK:
-        filter_query = {'_id': chat_id}
         credential_string_pickle = pickle.dumps(credential_string)
-        update_query = {'$set': {'credential_string': credential_string_pickle}}
-        gDrive.update_one(filter_query, update_query, upsert=True)
-
+        gDrive.set(chat_id, credential_string_pickle)
 
 def search(chat_id):
     with INSERTION_LOCK:
-        saved_cred = gDrive.find_one({'_id': chat_id})
+        credential_string_pickle = gDrive.get(chat_id)
         creds = None
-        if saved_cred is not None:
-            credential_string_pickle = saved_cred['credential_string']
+        if credential_string_pickle is not None:
             creds = pickle.loads(credential_string_pickle)
         return creds
 
-
 def _clear(chat_id):
     with INSERTION_LOCK:
-        gDrive.delete_one({'_id': chat_id})
+        gdrive.delete(chat_id)
