ce5c07f0f1cc62481022ff3c270879603b5bd0b4
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-16 22:24:03 +0630

    Update __init__.py

--- bot/helpers/sql_helper/__init__.py
+++ bot/helpers/sql_helper/__init__.py
@@ -1,11 +1,11 @@
-from pymongo import MongoClient
+import redis
 from bot import DATABASE_URL, LOGGER
 
 if DATABASE_URL is None:
     LOGGER.warning("DATABASE_URL is not set. The application cannot function without a database.")
     exit(1)
 
-client = MongoClient(DATABASE_URL)
-db = client["DRIVE_X"]
-parent_id = db["ParentID"]
-gDrive = db['gDriveCreds']
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+parent_id = redis_client
+gDrive = redis_client
