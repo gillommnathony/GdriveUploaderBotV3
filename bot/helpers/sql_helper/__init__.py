2d87f70e2f3854871179c3e39cf73e069fc9095b
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-16 22:35:53 +0630

    Update __init__.py

--- bot/helpers/sql_helper/__init__.py
+++ bot/helpers/sql_helper/__init__.py
@@ -5,7 +5,7 @@ if DATABASE_URL is None:
     LOGGER.warning("DATABASE_URL is not set. The application cannot function without a database.")
     exit(1)
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(DATABASE_URL)
 
 parent_id = redis_client
 gDrive = redis_client
