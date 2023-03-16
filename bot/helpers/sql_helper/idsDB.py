2afa554d0b55c30c54b69b24f1945d4758e31233
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-16 22:35:10 +0630

    Update idsDB.py

--- bot/helpers/sql_helper/idsDB.py
+++ bot/helpers/sql_helper/idsDB.py
@@ -1,17 +1,14 @@
 from bot.helpers.sql_helper import parent_id
 
 def search_parent(chat_id):
-    result = parent_id.find_one({"_id": chat_id})
-    if result:
-        return result["parent_id"]
+    parent_id_value = parent_id.get(chat_id)
+    if parent_id_value is not None:
+        return parent_id_value.decode('utf-8')
     else:
         return "root"
 
-
 def _set(chat_id, parent_id_value):
-    parent_id_object = {"_id": chat_id, "parent_id": parent_id_value}
-    parent_id.replace_one({"_id": chat_id}, parent_id_object, upsert=True)
-
+    parent_id.set(chat_id, parent_id_value)
 
 def _clear(chat_id):
-    parent_id.delete_one({"_id": chat_id})
+    parent_id.delete(chat_id)
